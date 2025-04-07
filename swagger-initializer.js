fdd716a9c9359ed5b7e0653e51b2b9ea36e7da02
masx200 <masx200@qq.com>
2025-04-07 14:38:05 +0800

    格式化

--- swagger-initializer.js
+++ swagger-initializer.js
@@ -1,9 +1,9 @@
-window.onload = function() {
+window.onload = function () {
   //<editor-fold desc="Changeable Configuration Block">
 
   // the following lines will be replaced by docker/configurator, when it runs in a docker-container
   window.ui = SwaggerUIBundle({
-    url: new URL("./openapi.json",location.href).href,
+    url: new URL("./openapi.json", location.href).href,
     dom_id: '#swagger-ui',
     deepLinking: true,
     presets: [
@@ -14,9 +14,9 @@ window.onload = function() {
       SwaggerUIBundle.plugins.DownloadUrl
     ],
     layout: "StandaloneLayout",
-      validatorUrl: "https://validator.swagger.io/validator",
-        queryConfigEnabled: true,
-      supportedSubmitMethods: ['get', 'put', 'post', 'delete', 'options', 'head', 'patch', 'trace', 'mkcol', 'propfind', 'proppatch', 'copy', 'move', 'lock', 'unlock', 'version-control', 'report', 'checkout', 'checkin', 'uncheckout', 'mkworkspace', 'update', 'label', 'merge', 'bind', 'unbind', 'orderpatch', 'acl'],
+    validatorUrl: "https://validator.swagger.io/validator",
+    queryConfigEnabled: true,
+    supportedSubmitMethods: ['get', 'put', 'post', 'delete', 'options', 'head', 'patch', 'trace', 'mkcol', 'propfind', 'proppatch', 'copy', 'move', 'lock', 'unlock', 'version-control', 'report', 'checkout', 'checkin', 'uncheckout', 'mkworkspace', 'update', 'label', 'merge', 'bind', 'unbind', 'orderpatch', 'acl'],
 
   });
 
